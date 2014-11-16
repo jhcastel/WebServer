36a26036a36ff8e896fdb3a43eb1bc5d6a61c5b4
JH <jhcastel@outlook.com>
2014-11-15 21:56:41 -0500

    model findone

--- pymes/models.py
+++ pymes/models.py
@@ -28,11 +28,11 @@ class user():
         	return self.UserID
 
         def get_user(self, raw_username): 
-        	query = self.find_one('username' : raw_username)
+        	query = self.find_one({'username' : raw_username})
         	return query
 
         def get_user_all(self, ID_num):
-            usr2 = self.find_one('_id' : int(ID_num))
+            usr2 = self.find_one({'_id' : int(ID_num)})
             return usr2
 
         def set_password(self, raw_password):
