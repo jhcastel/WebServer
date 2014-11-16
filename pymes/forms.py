17b73867e346a03d06dee32173e86dddafee1d07
JH <jhcastel@outlook.com>
2014-11-16 00:15:41 -0500

    forms add

--- pymes/forms.py
+++ pymes/forms.py
@@ -52,6 +52,7 @@ class LoanForm(forms.Form):
     rate = forms.DecimalField(required=True, label="Rate")
 
     def save(self, idadmin):
+        user1 = user()
         user2 = user1.get_user_all(idadmin)
         new_ltype=unicode(self.cleaned_data['category'])+":"+unicode(self.cleaned_data['rate'])
         user2['loantype'].append(new_ltype)
