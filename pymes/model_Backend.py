ac6c9fbe4a824fcde1975fa4908da07ca59b7187
JH <jhcastel@outlook.com>
2014-11-16 00:51:32 -0500

    dot modback

--- pymes/model_Backend.py
+++ pymes/model_Backend.py
@@ -64,7 +64,7 @@ def get_rates(us_id):
     user2 = user1.get_user_all(us_id)
     i = 1
     rate_dict = {}
-    for l in user2.['loantype']:
+    for l in user2['loantype']:
         a = l.split(":")[1]
         rate_dict[i] = a 
         i += 1
@@ -75,7 +75,7 @@ def id_cats(us_id):
     user2 = user1.get_user_all(us_id)
     i = 1
     cat_dict = {}
-    for l in user2.['loantype']:
+    for l in user2['loantype']:
         a = l.split(":")[0]
         cat_dict[i] = a 
         i += 1
