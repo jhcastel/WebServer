b55c8f9fd130748a59a9833679968bbd9f96f8cf
JH <jhcastel@outlook.com>
2014-11-17 13:35:23 -0500

    urls template

--- cloud/urls.py
+++ cloud/urls.py
@@ -11,6 +11,6 @@ urlpatterns = patterns('',
     url(r'^pymes/', include('pymes.urls', namespace="pymes")),
     #url(r'^simu/', include(admin.site.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^hirefire/','TemplateView', {'template': settings.MEDIA_ROOT, 'mimetype': 'text/plain'})
+    url(r'^hirefire/', TemplateView.as_view(template_name=settings.MEDIA_ROOT, content_type='text/plain')), #{'template': settings.MEDIA_ROOT, 'mimetype': 'text/plain'})
     #url(r'^hirefire/','serve', {'document_root': settings.MEDIA_ROOT}),
 )
