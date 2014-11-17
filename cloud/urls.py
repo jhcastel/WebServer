4c0e9915e27c632e6007c1cce797c92e1fb039b1
evelyncarovm <evelyncarovm@gmail.com>
2014-11-17 13:47:24 -0500

    resolucion conflicto

--- cloud/urls.py
+++ cloud/urls.py
@@ -10,10 +10,7 @@ urlpatterns = patterns('',
     url(r'^pymes/', include('pymes.urls', namespace="pymes")),
     #url(r'^simu/', include(admin.site.urls)),
     url(r'^admin/', include(admin.site.urls)),
-<<<<<<< HEAD
     url(r'^hirefire/7f53079738f19d64899f5c648a5c9db8e260cee5/info','serve', {'document_root': settings.MEDIA_ROOT}),
 )
-=======
 	#url(r'^hirefire/', include('views.hirefire', namespace="hirefire")),
-)
->>>>>>> ad29b7b71b5eff2b2d43b4a8c40b5ca31e884fd3
+
