577e3b24176a11a16306ff920d1cf5ce4a8628d6
JH <jhcastel@outlook.com>
2014-11-17 13:59:07 -0500

    urls cloud

--- cloud/urls.py
+++ cloud/urls.py
@@ -11,13 +11,5 @@ urlpatterns = patterns('',
     url(r'^pymes/', include('pymes.urls', namespace="pymes")),
     #url(r'^simu/', include(admin.site.urls)),
     url(r'^admin/', include(admin.site.urls)),
-<<<<<<< HEAD
-    url(r'^hirefire/7f53079738f19d64899f5c648a5c9db8e260cee5/info', TemplateView.as_view(template_name='info', content_type='application/json')), #{'template': settings.MEDIA_ROOT, 'mimetype': 'text/plain'})
-    #url(r'^hirefire/','serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^hirefire/7f53079738f19d64899f5c648a5c9db8e260cee5/info', TemplateView.as_view(template_name='info', content_type='application/json')),
 )
-=======
-    url(r'^hirefire/7f53079738f19d64899f5c648a5c9db8e260cee5/info','serve', {'document_root': settings.MEDIA_ROOT}),
-)
-	#url(r'^hirefire/', include('views.hirefire', namespace="hirefire")),
-
->>>>>>> 4c0e9915e27c632e6007c1cce797c92e1fb039b1
